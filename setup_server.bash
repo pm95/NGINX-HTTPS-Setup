b87d4637598f3a05cf2bac2401d74305c44da404
Pietro Malky <pietromalky@gmail.com>
2020-09-13 10:35:11 -0500

    modified bash script

--- setup_server.bash
+++ setup_server.bash
@@ -10,8 +10,9 @@ OS="$OSTYPE"
 DOMAIN="TEMP"
 SUB_DOMAIN="TEMP"
 TOP_LEVEL_DOMAIN="TEMP"
-BASE_CONFIG_FILE_PATH="/Users/pietromalky/Desktop" # CHANGE ME AFTER DEBUGGING
 FULL_CONFIG_FILE_PATH="TEMP"
+FULL_DOMAIN_NAME="TEMP"
+BASE_CONFIG_FILE_PATH="/Users/pietromalky/Desktop" # CHANGE ME AFTER DEBUGGING
 
 
 # check what operating system user is running
@@ -29,13 +30,20 @@ fi
 read -p "Enter your domain: " DOMAIN
 read -p "Enter your sub-domain: " SUB_DOMAIN
 read -p "Enter your top-level-domain: " TOP_LEVEL_DOMAIN
+FULL_DOMAIN_NAME="$SUB_DOMAIN.$DOMAIN.$TOP_LEVEL_DOMAIN"
 
 
 # echo domain back to user
-echo "Your fully qualified domain is: " "$SUB_DOMAIN"."$DOMAIN"."$TOP_LEVEL_DOMAIN"
+echo "Your fully qualified domain is: $FULL_DOMAIN_NAME" 
 
 
 # copy config file to target location
-FULL_CONFIG_FILE_PATH="$BASE_CONFIG_FILE_PATH/$SUB_DOMAIN.$DOMAIN.$TOP_LEVEL_DOMAIN.conf"
-cp "[sub domain].[domain].[top level domain].conf" $FULL_CONFIG_FILE_PATH
+FULL_CONFIG_FILE_PATH="$BASE_CONFIG_FILE_PATH/$FULL_DOMAIN_NAME.conf"
+cp "[sub domain].[domain].[top level domain].conf" "$FULL_CONFIG_FILE_PATH"
 echo "Created NGINX conf file at $FULL_CONFIG_FILE_PATH"
+
+
+# modify config file to include server name 
+SEARCH_STRING="[sub domain].[domain].[top level domain]"
+sed -i 's/$SEARCH_STRING/$FULL_DOMAIN_NAME/g' "$FULL_CONFIG_FILE_PATH"
+echo "Modified config file to fit your domain"
