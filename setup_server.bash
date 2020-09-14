beb8e6ef981351bb6f9423add76552c409db1acd
Pietro Malky <pietromalky@gmail.com>
2020-09-13 21:49:32 -0500

    added checks to see if certbot and nginx are installed

--- setup_server.bash
+++ setup_server.bash
@@ -10,27 +10,27 @@
 # ======== SETUP LOCAL VARIABLES ========
 # initial variable declarations
 OS="$OSTYPE"
-# DOMAIN="TEMP"
-# SUB_DOMAIN="TEMP"
-# TOP_LEVEL_DOMAIN="TEMP"
-# FULL_CONFIG_FILE_PATH="TEMP"
-# FULL_DOMAIN_NAME="TEMP"
-# SEARCH_STRING="_SUBDOMAIN_._DOMAIN_._TOPLEVELDOMAIN_"
-# STATIC_FILE_DIR_PATH="TEMP"
-# BASE_CONFIG_FILE_PATH="/Users/pietromalky/Desktop" # CHANGE ME AFTER DEBUGGING
+DOMAIN="TEMP"
+SUB_DOMAIN="TEMP"
+TOP_LEVEL_DOMAIN="TEMP"
+FULL_CONFIG_FILE_PATH="TEMP"
+FULL_DOMAIN_NAME="TEMP"
+SEARCH_STRING="_SUBDOMAIN_._DOMAIN_._TOPLEVELDOMAIN_"
+STATIC_FILE_DIR_PATH="TEMP"
+BASE_CONFIG_FILE_PATH="/Users/pietromalky/Desktop" # CHANGE ME AFTER DEBUGGING
 
-# # ======== GET USER INPUT ========
-# # read user input for domain, sub-domain and top-level-domain
-# read -p $'\e[36mEnter your domain (e.g. github): \e[0m' DOMAIN
-# read -p $'\e[36mEnter your sub-domain (e.g. www): \e[0m' SUB_DOMAIN
-# read -p $'\e[36mEnter your top-level-domain (e.g. com): \e[0m' TOP_LEVEL_DOMAIN
-# FULL_DOMAIN_NAME="$SUB_DOMAIN.$DOMAIN.$TOP_LEVEL_DOMAIN"
+# ======== GET USER INPUT ========
+# read user input for domain, sub-domain and top-level-domain
+read -p $'\e[36mEnter your domain (e.g. github): \e[0m' DOMAIN
+read -p $'\e[36mEnter your sub-domain (e.g. www): \e[0m' SUB_DOMAIN
+read -p $'\e[36mEnter your top-level-domain (e.g. com): \e[0m' TOP_LEVEL_DOMAIN
+FULL_DOMAIN_NAME="$SUB_DOMAIN.$DOMAIN.$TOP_LEVEL_DOMAIN"
 
 
-# # echo domain back to user
-# yellow='\e[33m'
-# nc='\e[0m'
-# echo -e $'\n\e[36mYour full domain is: ' $'\e[33m' "$FULL_DOMAIN_NAME" $'\e[0m'
+# echo domain back to user
+yellow='\e[33m'
+nc='\e[0m'
+echo -e $'\n\e[36mYour full domain is: ' $'\e[33m' "$FULL_DOMAIN_NAME" $'\e[0m'
 
 
 # ======== OS-specific command generation ========
@@ -76,36 +76,28 @@ fi
 
 
 
-# # copy config file to target location
-# FULL_CONFIG_FILE_PATH="$BASE_CONFIG_FILE_PATH/$FULL_DOMAIN_NAME.conf"
-# cp "[sub domain].[domain].[top level domain].conf" "$FULL_CONFIG_FILE_PATH"
-# echo -e $'\e[36mCreated NGINX conf file at: ' $'\e[33m' "$FULL_CONFIG_FILE_PATH" $'\e[0m'
+# copy config file to target location
+FULL_CONFIG_FILE_PATH="$BASE_CONFIG_FILE_PATH/$FULL_DOMAIN_NAME.conf"
+cp "[sub domain].[domain].[top level domain].conf" "$FULL_CONFIG_FILE_PATH"
+echo -e $'\e[36mCreated NGINX conf file at: ' $'\e[33m' "$FULL_CONFIG_FILE_PATH" $'\e[0m'
 
 
-# # add path where static files will be served from
-# read -p $'\n\e[36mFull path to directory containing static files you want to serve: \e[0m' STATIC_FILE_DIR_PATH
+# add path where static files will be served from
+read -p $'\n\e[36mFull path to directory containing static files you want to serve: \e[0m' STATIC_FILE_DIR_PATH
 
 
-# # modify config file to include server name based on user's OS
-# if [[ "$OS" == "linux-gnu"* ]]; then
-#     sed -i "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
-#     sed -i "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
-# elif [[ "$OS" == "darwin"* ]]; then
-#     sed -i '' "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
-#     sed -i '' "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
-# else
-#     echo -e $'\e[31mCannot run setup script on your OS:' "$FULL_CONFIG_FILE_PATH" $'. Exiting now\e[0m'
-#     exit 1
-# fi
-# echo -e $'\n\e[36mCreated NGINX config file to fit your domain\e[0m'
+# modify config file to include server name based on user's OS
+$SED "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
+$SED "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
+echo -e $'\n\e[36mCreated NGINX config file to fit your domain\e[0m'
 
 
-# # ask user if they have API's they want to add to the proxy passthrough
+# ask user if they have API's they want to add to the proxy passthrough
 
 
 
-# # ======== NGINX SERVER RUNNING HANDLING ========
-# # kill all current processes on port 80
+# ======== NGINX SERVER RUNNING HANDLING ========
+# kill all current processes on port 80
 
 
-# # run NGINX server
+# run NGINX server
