c2d5cbff1c8f0568630560e692d907af41887b37
Pietro Malky <pietromalky@gmail.com>
2020-09-13 21:48:38 -0500

    added checks to see if certbot and nginx are installed

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NGINX-HTTPS-Setup
+
+## NGINX
+
+### Config file location on machine
+
+/etc/nginx/conf.d/_SUBDOMAIN_._DOMAIN_._TOPLEVELDOMAIN_.conf
+
+- make sure to first obtain your certificate with Let's Encrypt and Certbot
+  - sudo certbot certonly --standalone -d _SUBDOMAIN_._DOMAIN_._TOPLEVELDOMAIN_
+- If you don't have Certbot or NGINX on your system, and you're running Ubuntu:
+  - sudo apt-get install certbot
+  - sudo apt-get install nginx
+- Kill any processes running on port 80:
+  - sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+
+<!-- # Generated with Mozilla's SSL configuration site
+
+# generated 2020-09-04, Mozilla Guideline v5.6, nginx 1.14.0, OpenSSL 1.1.1, modern configuration
+
+# https://ssl-config.mozilla.org/#server=nginx&version=1.14.0&config=modern&openssl=1.1.1&guideline=5.6 -->
--- setup_server.bash
+++ setup_server.bash
@@ -10,73 +10,102 @@
 # ======== SETUP LOCAL VARIABLES ========
 # initial variable declarations
 OS="$OSTYPE"
-DOMAIN="TEMP"
-SUB_DOMAIN="TEMP"
-TOP_LEVEL_DOMAIN="TEMP"
-FULL_CONFIG_FILE_PATH="TEMP"
-FULL_DOMAIN_NAME="TEMP"
-SEARCH_STRING="_SUBDOMAIN_._DOMAIN_._TOPLEVELDOMAIN_"
-STATIC_FILE_DIR_PATH="TEMP"
-BASE_CONFIG_FILE_PATH="/Users/pietromalky/Desktop" # CHANGE ME AFTER DEBUGGING
+# DOMAIN="TEMP"
+# SUB_DOMAIN="TEMP"
+# TOP_LEVEL_DOMAIN="TEMP"
+# FULL_CONFIG_FILE_PATH="TEMP"
+# FULL_DOMAIN_NAME="TEMP"
+# SEARCH_STRING="_SUBDOMAIN_._DOMAIN_._TOPLEVELDOMAIN_"
+# STATIC_FILE_DIR_PATH="TEMP"
+# BASE_CONFIG_FILE_PATH="/Users/pietromalky/Desktop" # CHANGE ME AFTER DEBUGGING
 
-# ======== GET USER INPUT ========
-# read user input for domain, sub-domain and top-level-domain
-read -p $'\e[36mEnter your domain (e.g. github): \e[0m' DOMAIN
-read -p $'\e[36mEnter your sub-domain (e.g. www): \e[0m' SUB_DOMAIN
-read -p $'\e[36mEnter your top-level-domain (e.g. com): \e[0m' TOP_LEVEL_DOMAIN
-FULL_DOMAIN_NAME="$SUB_DOMAIN.$DOMAIN.$TOP_LEVEL_DOMAIN"
-
-
-# echo domain back to user
-yellow='\e[33m'
-nc='\e[0m'
-echo -e $'\n\e[36mYour full domain is: ' $'\e[33m' "$FULL_DOMAIN_NAME" $'\e[0m'
+# # ======== GET USER INPUT ========
+# # read user input for domain, sub-domain and top-level-domain
+# read -p $'\e[36mEnter your domain (e.g. github): \e[0m' DOMAIN
+# read -p $'\e[36mEnter your sub-domain (e.g. www): \e[0m' SUB_DOMAIN
+# read -p $'\e[36mEnter your top-level-domain (e.g. com): \e[0m' TOP_LEVEL_DOMAIN
+# FULL_DOMAIN_NAME="$SUB_DOMAIN.$DOMAIN.$TOP_LEVEL_DOMAIN"
 
 
+# # echo domain back to user
+# yellow='\e[33m'
+# nc='\e[0m'
+# echo -e $'\n\e[36mYour full domain is: ' $'\e[33m' "$FULL_DOMAIN_NAME" $'\e[0m'
 
 
-# ======== CERTBOT CERTIFICATE CREATION HANDLING ========
-# check certbot is installed on machine, if not, install it
+# ======== OS-specific command generation ========
+# package manager
+PKG_QUERY="TEMP"
 
+# sed command varies from MacOS to Linux
+SED="TEMP"
 
-
-
-# ======== NGINX CONFIG FILE HANDLING ========
-# check nginx is installed on machine, if not, install it
-
-
-
-
-# copy config file to target location
-FULL_CONFIG_FILE_PATH="$BASE_CONFIG_FILE_PATH/$FULL_DOMAIN_NAME.conf"
-cp "[sub domain].[domain].[top level domain].conf" "$FULL_CONFIG_FILE_PATH"
-echo -e $'\e[36mCreated NGINX conf file at: ' $'\e[33m' "$FULL_CONFIG_FILE_PATH" $'\e[0m'
-
-
-# add path where static files will be served from
-read -p $'\n\e[36mFull path to directory containing static files you want to serve: \e[0m' STATIC_FILE_DIR_PATH
-
-
-# modify config file to include server name based on user's OS
 if [[ "$OS" == "linux-gnu"* ]]; then
-    sed -i "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
-    sed -i "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
+    PKG_QUERY="dpkg-query --list"
+    SED="sed -i"
 elif [[ "$OS" == "darwin"* ]]; then
-    sed -i '' "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
-    sed -i '' "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
+    PKG_QUERY="brew list"
+    SED="sed -i ''"
 else
     echo -e $'\e[31mCannot run setup script on your OS:' "$FULL_CONFIG_FILE_PATH" $'. Exiting now\e[0m'
     exit 1
 fi
-echo -e $'\n\e[36mCreated NGINX config file to fit your domain\e[0m'
 
 
-# ask user if they have API's they want to add to the proxy passthrough
+# # ======== CERTBOT CERTIFICATE CREATION HANDLING ========
+# # check certbot is installed on machine, if not, install it
+CERTBOT_COUNT=`$PKG_QUERY | grep -i -c -w "certbot"`
+if [[ "$CERTBOT_COUNT" > 0 ]]; then 
+    echo "Certbot is installed"
+else    
+    echo "Certbot is NOT installed"
+    exit 1
+fi
 
 
 
-# ======== NGINX SERVER RUNNING HANDLING ========
-# kill all current processes on port 80
+# # ======== NGINX CONFIG FILE HANDLING ========
+# # check nginx is installed on machine, if not, install it
+NGINX_COUNT=`$PKG_QUERY | grep -i -c -w "nginx"`
+if [[ "$NGINX_COUNT" > 0 ]]; then 
+    echo "NGINX is installed"
+else    
+    echo "NGINX is NOT installed"
+    exit 1
+fi
 
 
-# run NGINX server
+
+# # copy config file to target location
+# FULL_CONFIG_FILE_PATH="$BASE_CONFIG_FILE_PATH/$FULL_DOMAIN_NAME.conf"
+# cp "[sub domain].[domain].[top level domain].conf" "$FULL_CONFIG_FILE_PATH"
+# echo -e $'\e[36mCreated NGINX conf file at: ' $'\e[33m' "$FULL_CONFIG_FILE_PATH" $'\e[0m'
+
+
+# # add path where static files will be served from
+# read -p $'\n\e[36mFull path to directory containing static files you want to serve: \e[0m' STATIC_FILE_DIR_PATH
+
+
+# # modify config file to include server name based on user's OS
+# if [[ "$OS" == "linux-gnu"* ]]; then
+#     sed -i "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
+#     sed -i "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
+# elif [[ "$OS" == "darwin"* ]]; then
+#     sed -i '' "s/$SEARCH_STRING/$FULL_DOMAIN_NAME/g" "$FULL_CONFIG_FILE_PATH"
+#     sed -i '' "s+_STATICFILEDIRECTORY_+$STATIC_FILE_DIR_PATH+g" "$FULL_CONFIG_FILE_PATH"
+# else
+#     echo -e $'\e[31mCannot run setup script on your OS:' "$FULL_CONFIG_FILE_PATH" $'. Exiting now\e[0m'
+#     exit 1
+# fi
+# echo -e $'\n\e[36mCreated NGINX config file to fit your domain\e[0m'
+
+
+# # ask user if they have API's they want to add to the proxy passthrough
+
+
+
+# # ======== NGINX SERVER RUNNING HANDLING ========
+# # kill all current processes on port 80
+
+
+# # run NGINX server
